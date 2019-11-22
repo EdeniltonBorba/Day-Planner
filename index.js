1b6a8af7a6069a49a57147e25a6dd93ad3938bbc
Edenilton <edeniltonborba.adm@gmail.com>
2019-11-22 18:09:57 +0100

    localStorage

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 var container = document.querySelector("#calendarList");
-var currentHour = parseInt(moment().format("H"));
+var currentHour = parseInt(moment().format('H'));
+
 
 for (var i=9; i <= 17; i++) {
     var className;
-    var LsVal = localStorage.getItem(`userInput-${i}`);
+    var lsVal = localStorage.getItem(`userInput-${i}`);
     if (i === currentHour) {
-        className = "present";
+        className = 'present';
     } else if (i < currentHour) {
-        className = "past";
+        className = 'past';
     } else {
-        className = " future";
+        className = 'future';
     }
-
     container.innerHTML += `
         <li class="row ${className}">
-           <span class="time col-md-1">${i}</span>
-           <input id="input-${i}" type="text" class="col-md-10 userInput" value="${lsVal || ''}" />
-           <button data-hour="${i}" class="save col-md-1">Save</button>
+          <span class="time col-md-1">${i}</span>
+          <input id="input-${i}" type="text" class="col-md-10 userInput" value="${lsVal || ''}" />
+          <button data-hour="${i}" class="save col-md-1">Save</button>
         </li>
     `;
 }
-
-document.querySelector(".save").addEventListener("click", function(event) {
-    var hour = $(event.target).data("hour");
-    var userInput = $(`#input-` + hour).val();
-    localStorage.setItem("userInput-" + hour, userInput);
+document.querySelector('.save').addEventListener('click', function(event) {
+    var hour = $(event.target).data('hour');
+    var userInput = $('#input-' + hour).val();
+    localStorage.setItem('userInput-' + hour, userInput);
 })
